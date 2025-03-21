b3680aa0270e0d67b8b12f2b7761ae8dccfe2ac6
Truong Nhat Nguyen <tnhatnguyen.dev2805@gmail.com>
2025-03-21 14:57:28 +0700

    Add project files.

---
+++ Cinema_Management_System/AdminForm.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Cinema_Management_System
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.StatisticalManage_Btn = new System.Windows.Forms.Button();
+            this.Logout_Btn = new System.Windows.Forms.Button();
+            this.PersonManage_Btn = new System.Windows.Forms.Button();
+            this.VoucherManage_Btn = new System.Windows.Forms.Button();
+            this.ProductManage_Btn = new System.Windows.Forms.Button();
+            this.CustomerManage_Btn = new System.Windows.Forms.Button();
+            this.StaffManage_Btn = new System.Windows.Forms.Button();
+            this.ShowTimeManage_Btn = new System.Windows.Forms.Button();
+            this.MovieManage_Btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.staffManageForm1 = new Cinema_Management_System.StaffManageForm();
+            this.account_Form1 = new Cinema_Management_System.Account_Form();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.StatisticalManage_Btn);
+            this.panel2.Controls.Add(this.Logout_Btn);
+            this.panel2.Controls.Add(this.PersonManage_Btn);
+            this.panel2.Controls.Add(this.VoucherManage_Btn);
+            this.panel2.Controls.Add(this.ProductManage_Btn);
+            this.panel2.Controls.Add(this.CustomerManage_Btn);
+            this.panel2.Controls.Add(this.StaffManage_Btn);
+            this.panel2.Controls.Add(this.ShowTimeManage_Btn);
+            this.panel2.Controls.Add(this.MovieManage_Btn);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(222, 803);
+            this.panel2.TabIndex = 1;
+            // 
+            // StatisticalManage_Btn
+            // 
+            this.StatisticalManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatisticalManage_Btn.Location = new System.Drawing.Point(0, 546);
+            this.StatisticalManage_Btn.Name = "StatisticalManage_Btn";
+            this.StatisticalManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.StatisticalManage_Btn.TabIndex = 10;
+            this.StatisticalManage_Btn.Text = "Thống Kê";
+            this.StatisticalManage_Btn.UseVisualStyleBackColor = true;
+            // 
+            // Logout_Btn
+            // 
+            this.Logout_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Logout_Btn.Location = new System.Drawing.Point(0, 727);
+            this.Logout_Btn.Name = "Logout_Btn";
+            this.Logout_Btn.Size = new System.Drawing.Size(222, 49);
+            this.Logout_Btn.TabIndex = 9;
+            this.Logout_Btn.Text = "Đăng Xuất";
+            this.Logout_Btn.UseVisualStyleBackColor = true;
+            // 
+            // PersonManage_Btn
+            // 
+            this.PersonManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PersonManage_Btn.Location = new System.Drawing.Point(0, 657);
+            this.PersonManage_Btn.Name = "PersonManage_Btn";
+            this.PersonManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.PersonManage_Btn.TabIndex = 8;
+            this.PersonManage_Btn.Text = "Cá Nhân";
+            this.PersonManage_Btn.UseVisualStyleBackColor = true;
+            this.PersonManage_Btn.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // VoucherManage_Btn
+            // 
+            this.VoucherManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VoucherManage_Btn.Location = new System.Drawing.Point(3, 475);
+            this.VoucherManage_Btn.Name = "VoucherManage_Btn";
+            this.VoucherManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.VoucherManage_Btn.TabIndex = 7;
+            this.VoucherManage_Btn.Text = "Voucher";
+            this.VoucherManage_Btn.UseVisualStyleBackColor = true;
+            // 
+            // ProductManage_Btn
+            // 
+            this.ProductManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProductManage_Btn.Location = new System.Drawing.Point(0, 404);
+            this.ProductManage_Btn.Name = "ProductManage_Btn";
+            this.ProductManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.ProductManage_Btn.TabIndex = 6;
+            this.ProductManage_Btn.Text = "Quản Lý Sản Phẩm";
+            this.ProductManage_Btn.UseVisualStyleBackColor = true;
+            // 
+            // CustomerManage_Btn
+            // 
+            this.CustomerManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CustomerManage_Btn.Location = new System.Drawing.Point(0, 333);
+            this.CustomerManage_Btn.Name = "CustomerManage_Btn";
+            this.CustomerManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.CustomerManage_Btn.TabIndex = 5;
+            this.CustomerManage_Btn.Text = "Quản Lý Khách Hàng";
+            this.CustomerManage_Btn.UseVisualStyleBackColor = true;
+            // 
+            // StaffManage_Btn
+            // 
+            this.StaffManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StaffManage_Btn.Location = new System.Drawing.Point(0, 262);
+            this.StaffManage_Btn.Name = "StaffManage_Btn";
+            this.StaffManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.StaffManage_Btn.TabIndex = 4;
+            this.StaffManage_Btn.Text = "Quản Lý Nhân Sự";
+            this.StaffManage_Btn.UseVisualStyleBackColor = true;
+            this.StaffManage_Btn.Click += new System.EventHandler(this.MovieManage_Btn_Click);
+            // 
+            // ShowTimeManage_Btn
+            // 
+            this.ShowTimeManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ShowTimeManage_Btn.Location = new System.Drawing.Point(0, 191);
+            this.ShowTimeManage_Btn.Name = "ShowTimeManage_Btn";
+            this.ShowTimeManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.ShowTimeManage_Btn.TabIndex = 3;
+            this.ShowTimeManage_Btn.Text = "Quản Lý Suất Chiếu";
+            this.ShowTimeManage_Btn.UseVisualStyleBackColor = true;
+            // 
+            // MovieManage_Btn
+            // 
+            this.MovieManage_Btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieManage_Btn.Location = new System.Drawing.Point(0, 120);
+            this.MovieManage_Btn.Name = "MovieManage_Btn";
+            this.MovieManage_Btn.Size = new System.Drawing.Size(222, 49);
+            this.MovieManage_Btn.TabIndex = 2;
+            this.MovieManage_Btn.Text = "Quản Lý Phim";
+            this.MovieManage_Btn.UseVisualStyleBackColor = true;
+            this.MovieManage_Btn.Click += new System.EventHandler(this.MovieManage_Btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Logo nè";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.staffManageForm1);
+            this.panel3.Controls.Add(this.account_Form1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(222, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1001, 803);
+            this.panel3.TabIndex = 3;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // staffManageForm1
+            // 
+            this.staffManageForm1.Location = new System.Drawing.Point(0, 0);
+            this.staffManageForm1.Name = "staffManageForm1";
+            this.staffManageForm1.Size = new System.Drawing.Size(1001, 803);
+            this.staffManageForm1.TabIndex = 1;
+            // 
+            // account_Form1
+            // 
+            this.account_Form1.Location = new System.Drawing.Point(0, 0);
+            this.account_Form1.Name = "account_Form1";
+            this.account_Form1.Size = new System.Drawing.Size(1001, 803);
+            this.account_Form1.TabIndex = 0;
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 803);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "AdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button MovieManage_Btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button Logout_Btn;
+        private System.Windows.Forms.Button PersonManage_Btn;
+        private System.Windows.Forms.Button VoucherManage_Btn;
+        private System.Windows.Forms.Button ProductManage_Btn;
+        private System.Windows.Forms.Button CustomerManage_Btn;
+        private System.Windows.Forms.Button StaffManage_Btn;
+        private System.Windows.Forms.Button ShowTimeManage_Btn;
+        private System.Windows.Forms.Button StatisticalManage_Btn;
+        private Account_Form account_Form1;
+        private StaffManageForm staffManageForm1;
+    }
+}
+
---
+++ Cinema_Management_System/AdminForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cinema_Management_System
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            staffManageForm1.Hide();
+            account_Form1.Show();
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            account_Form1.Hide();
+            staffManageForm1.Show();     
+        }
+
+        private void MovieManage_Btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
